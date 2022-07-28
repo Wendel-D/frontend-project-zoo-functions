4bdf9e761c389f3548c1b53019be4f5d72985627
Wendel Daniel <wendeldaniel263@gmail.com>
2022-07-28 16:42:18 -0300

    exercicio 4 ok

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((elemento) => elemento.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const a = employees.filter((elemento) => elemento.managers.includes(managerId));
+    return a.map((nomes) => `${nomes.firstName} ${nomes.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
