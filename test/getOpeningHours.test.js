a55134afdb6520c59f76c03f37606e6278908460
Wendel Daniel <wendeldaniel263@gmail.com>
2022-07-28 19:15:17 -0300

    teste elefante ok

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,16 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+    it('return test', () => {
+        expect(getOpeningHours()).toEqual({
+            Tuesday: {open: 8, close: 6 },
+            Wednesday: { open: 8, close: 6 },
+            Thusday: { open: 10, close: 8 },
+            Friday: { open: 10, close: 8 },
+            Saturday: {open: 8, close: 10 },
+            Sunday: { open: 8, close: 8 },
+            Monday: { open: 8, close: 8 },
+        });
+    })
+    
+});
