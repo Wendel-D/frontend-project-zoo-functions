a670d8f4a67e63e65a17674f462a8e741d204ab8
Wendel Daniel <wendeldaniel263@gmail.com>
2022-07-28 19:32:00 -0300

    tests ok

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,16 +1,34 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {
-    it('return test', () => {
+    it('testar o horario', () => {
         expect(getOpeningHours()).toEqual({
             Tuesday: {open: 8, close: 6 },
             Wednesday: { open: 8, close: 6 },
-            Thusday: { open: 10, close: 8 },
+            Thursday: { open: 10, close: 8 },
             Friday: { open: 10, close: 8 },
             Saturday: {open: 8, close: 10 },
             Sunday: { open: 8, close: 8 },
-            Monday: { open: 8, close: 8 },
+            Monday: { open: 0, close: 0 },
         });
     })
-    
+    it('Teste de aviso Fechado', () => {
+        expect(getOpeningHours('Monday', '12:30-AM')).toBe('The zoo is closed');
+    })
+    it('Teste de aviso Aberto', () => {
+        expect(getOpeningHours('Thursday', '10:30-AM')).toBe('The zoo is open');
+    })
+    it('Testando validade de dia', () => {
+        expect(() => getOpeningHours('every')).toThrowError('The day must be valid. Example: Monday');
+    })
 });
+
+describe('Testando dias diferentes', () => {
+    it('Dias diferentes', () => {
+        expect(() => getOpeningHours('Monday', '12:30-AC')).toThrow();
+        expect(() => getOpeningHours('Monday', '12:AC-AM')).toThrow();
+        expect(() => getOpeningHours('Monday', 'E1:30-AM')).toThrow();
+        expect(() => getOpeningHours('Monday', '13:30-AM')).toThrow();
+        expect(() => getOpeningHours('Monday', '24:88-AM')).toThrowError();
+    })
+})
