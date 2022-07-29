b42810dbabdec19f463dcadb31f4b58fa251e8f1
Wendel Daniel <wendeldaniel263@gmail.com>
2022-07-29 15:47:24 -0300

    exercicio 10 ok

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
+const { name } = require('faker/locale/pt_BR'); // APAGAR DEPOIS
+
+const entrants = [
+  { name: name.findName(), age: 5 },
+  { name: name.findName(), age: 5 },
+  { name: name.findName(), age: 5 },
+  { name: name.findName(), age: 18 },
+  { name: name.findName(), age: 18 },
+  { name: name.findName(), age: 50 },
+];
 
 function countEntrants(entrants) {
-  // seu código aqui
+  entrants.reduce((acumulador, valorAtual) => {
+    ['adult'] = valorAtual.age;
+    return ;
+  }, {})
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+// function calculateEntry(entrants) {
+  
+// }
 
-module.exports = { calculateEntry, countEntrants };
+console.log(countEntrants(entrants));
+// module.exports = { calculateEntry, countEntrants };
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const colab = employees.find((elemento) => elemento.id === id);
+  const firstSpecie = species.find((elemento) => elemento.id === colab.responsibleFor[0]);
+  const oldestAnimal = firstSpecie.residents.sort((a, b) => b.age - a.age);
+  return Object.values(oldestAnimal[0]);
 }
-
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 module.exports = getOldestFromFirstSpecies;
