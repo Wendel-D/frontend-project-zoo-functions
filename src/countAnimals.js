8cf14bde516ab2b0ebbf32d0b57f554eced614b6
Wendel Daniel <wendeldaniel263@gmail.com>
2022-07-29 14:27:27 -0300

    exercicio 5 ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const animais = species.reduce((acc, currentValue) => {
+      acc[currentValue.name] = currentValue.residents.length;
+      return acc;
+    }, {});
+    return animais;
+  } if (animal.sex === undefined) {
+    const animais = species.find((elemento) => elemento.name === animal.specie);
+    const animaisQt = animais.residents.length;
+    return animaisQt;
+  }
+  const animais = species.find((elemento) => elemento.name === animal.specie);
+  return animais.residents.filter((elemento) => elemento.sex === animal.sex).length;
 }
 
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
